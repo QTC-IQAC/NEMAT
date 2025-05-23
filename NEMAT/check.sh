a387872dff819c71c0b19520e804b4d614efdcc7
Albert2424 <albertixortega@gmail.com>
2025-05-23 13:11:27 +0200

    Updated check.sh so it doesn't fail when warnings/error file doesn't exist

--- NEMAT/check.sh
+++ NEMAT/check.sh
@@ -3,9 +3,14 @@
 file=$1
 
 echo ""
-cat logs/warnings_$file.log
-echo ""
-echo ""
-cat logs/errors_$file.log
-echo ""
+if [ -f "logs/warnings_$file.log" ]; then
+    cat logs/warnings_$file.log
+    echo ""
+    echo ""
+
+    cat logs/errors_$file.log
+    echo ""
+else
+    echo -e "logs/warnings_$file.log doesn't exist, \033[31man error may have occurred before finishing\033[0m. Check logs/$file.err for errors."
+fi
 
