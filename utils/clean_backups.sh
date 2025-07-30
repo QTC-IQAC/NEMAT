f67c686fb3ae6a9d365545fcaead5f31a6143e95
Albert2424 <albertixortega@gmail.com>
2025-07-30 09:16:32 +0200

    updated clean backups so that the right message is displayed

--- utils/clean_backups.sh
+++ utils/clean_backups.sh
@@ -6,7 +6,7 @@ nfiles=$(find . -type f -name '#*' 2>/dev/null | wc -l)
 memfiles=$(find . -type f -name '#*' -print0 | du --files0-from=- -ch | awk '/total$/ {print $1}')
 
 if [ $nfiles -gt 0 ]; then
-    echo -e "Found \033[31m$nfiles\033[0m backup files, consuming \033[31m$memfiles\033[0m. Do you want to continue? (yes/no): "
+    echo -e "Found \033[31m$nfiles\033[0m backup files, consuming \033[31m$memfiles\033[0m. Do you want to remove backup files? (yes/no): "
     read answer
     answer=${answer,,}  # convert to lowercase
     
